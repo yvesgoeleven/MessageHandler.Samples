0e9cf8820a0bfb96e98970a96cac248c74a6db4a
Yves Goeleven <yves@goeleven.com>
2014-03-25 12:24:19 +0100

    added sample githubclien

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+using Octokit.Internal;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.WaitAll(MainAsync(args));
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var github = Build(Credentials.Anonymous);
+
+            var newIssue = new NewIssue("Some issue")
+            {
+                Body = "Some body"
+            };
+
+            await github.Issue.Create("MessageHandler", "MessageHandler.Samples", newIssue);
+
+        }
+
+        public static GitHubClient Build(Credentials credentials)
+        {
+            var credentialStore = new InMemoryCredentialStore(credentials);
+
+            var httpClient = new HttpClientAdapter();
+
+            var connection = new Connection(
+                new ProductHeaderValue("MessageHandler"),
+                GitHubClient.GitHubApiUrl,
+                credentialStore,
+                httpClient,
+                new SimpleJsonSerializer());
+
+            var client = new GitHubClient(connection);
+
+            return client;
+        }
+    }
+}
